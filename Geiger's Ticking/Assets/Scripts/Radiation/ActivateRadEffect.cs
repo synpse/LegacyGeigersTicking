2985487f4cd49fa02025a0e656585478923034ca
synpse <tfaon@sapo.pt>
2018-12-11 18:03:16 +0000

    Radiation Zones Overhaul

--- Geiger's Ticking/Assets/Scripts/Radiation/ActivateRadEffect.cs
+++ Geiger's Ticking/Assets/Scripts/Radiation/ActivateRadEffect.cs
@@ -8,89 +8,107 @@ public class ActivateRadEffect : MonoBehaviour {
     // Header for the sake of organization
     [Header("Components")]
 
-    [SerializeField] private AudioSource sound;
-    [SerializeField] private PostProcessingProfile ppp;
-    [SerializeField] private SphereCollider RadiationLow;
-    [SerializeField] private SphereCollider RadiationMedium;
-    [SerializeField] private SphereCollider RadiationHigh;
-    [SerializeField] private SphereCollider RadiationExtreme;
+    [SerializeField] private PostProcessingProfile _ppp;
+    [SerializeField] private GameObject _player;
+    [SerializeField] private AudioSource _sound;
+    [SerializeField] private AudioClip _radLow;
+    [SerializeField] private AudioClip _radMed;
+    [SerializeField] private AudioClip _radHigh;
+    [SerializeField] private AudioClip _radExtr;
 
     [HideInInspector] public static float radsAccumulated;
 
-    GrainModel.Settings grain;
-    ChromaticAberrationModel.Settings aberration;
+    private float radIntensity;
+    private float radZoneRadius;
 
-    private void OnTriggerEnter(Collider col)
-    {
-        if (col.gameObject.tag == "Player")
-        {
-            sound.Play();
-            sound.loop = true;
+    private bool inRadZoneLow;
+    private bool inRadZoneMed;
+    private bool inRadZoneHigh;
+    private bool inRadZoneExtr;
 
-            if (gameObject.tag == "RadZoneLow")
-            {
-                grain.intensity = 0.3f;
-                grain.size = 1.4f;
-                aberration.intensity = 0.25f;
-                ppp.grain.settings = grain;
-                ppp.chromaticAberration.settings = aberration;
-                Debug.Log("Radiation intensity: LOW");
-            }
-            if (gameObject.tag == "RadZoneMedium")
-            {
-                RadiationLow.enabled = false;
-                grain.intensity = 0.5f;
-                grain.size = 1.6f;
-                aberration.intensity = 0.55f;
-                ppp.grain.settings = grain;
-                ppp.chromaticAberration.settings = aberration;
-                Debug.Log("Radiation intensity: MEDIUM");
-            }
-            if (gameObject.tag == "RadZoneHigh")
-            {
-                RadiationLow.enabled = false;
-                RadiationMedium.enabled = false;
-                grain.intensity = 0.7f;
-                grain.size = 1.8f;
-                aberration.intensity = 0.85f;
-                ppp.grain.settings = grain;
-                ppp.chromaticAberration.settings = aberration;
-                Debug.Log("Radiation intensity: HIGH");
-            }
-            if (gameObject.tag == "RadZoneExtreme")
-            {
-                RadiationLow.enabled = false;
-                RadiationMedium.enabled = false;
-                RadiationHigh.enabled = false;
-                grain.intensity = 1f;
-                grain.size = 2f;
-                aberration.intensity = 1f;
-                ppp.grain.settings = grain;
-                ppp.chromaticAberration.settings = aberration;
-                Debug.Log("Radiation intensity: EXTREME");
-            }
-        }
-    }
+    private GrainModel.Settings _grain;
+    private ChromaticAberrationModel.Settings _aberration;
 
     private void OnTriggerStay(Collider col)
     {
         if (col.gameObject.tag == "Player")
         {
-            if (gameObject.tag == "RadZoneLow")
+            if (gameObject.tag == "RadZone")
             {
-                radsAccumulated += Random.Range(0.0f, 0.25f);
-            }
-            if (gameObject.tag == "RadZoneMedium")
-            {
-                radsAccumulated += Random.Range(0.0f, 0.5f);
-            }
-            if (gameObject.tag == "RadZoneHigh")
-            {
-                radsAccumulated += Random.Range(0.0f, 0.75f);
-            }
-            if (gameObject.tag == "RadZoneExtreme")
-            {
-                radsAccumulated += Random.Range(0.0f, 1f);
+                radZoneRadius = (gameObject.transform.position - _player.transform.position).magnitude;
+
+                // The feedback should decrease with distance from the center.
+                radIntensity = 1f / radZoneRadius;
+
+                _grain.intensity = radIntensity * 2f;
+                _grain.size = 2f;
+                _aberration.intensity = 0;
+                _ppp.grain.settings = _grain;
+                _ppp.chromaticAberration.settings = _aberration;
+
+                if (radIntensity > 0f && radIntensity <= 0.2f)
+                {
+                    radsAccumulated += Random.Range(0f, 0.2f);
+
+                    inRadZoneMed = false;
+                    inRadZoneHigh = false;
+                    inRadZoneExtr = false;
+
+                    if (inRadZoneLow == false)
+                    {
+                        _sound.clip = _radLow;
+                        _sound.Play();
+                        inRadZoneLow = true;
+                    }
+                }
+
+                if (radIntensity > 0.2f && radIntensity <= 0.4f)
+                {
+                    radsAccumulated += Random.Range(0.2f, 0.4f);
+
+                    inRadZoneLow = false;
+                    inRadZoneHigh = false;
+                    inRadZoneExtr = false;
+
+                    if (inRadZoneMed == false)
+                    {
+                        _sound.clip = _radMed;
+                        _sound.Play();
+                        inRadZoneMed = true;
+                    }
+                }
+
+                if (radIntensity > 0.4f && radIntensity <= 0.6f)
+                {
+                    radsAccumulated += Random.Range(0.4f, 0.6f);
+
+                    inRadZoneLow = false;
+                    inRadZoneMed = false;
+                    inRadZoneExtr = false;
+
+                    if (inRadZoneHigh == false)
+                    {
+                        _sound.clip = _radHigh;
+                        _sound.Play();
+                        inRadZoneHigh = true;
+                    }
+                }
+
+                if (radIntensity > 0.6f)
+                {
+                    radsAccumulated += Random.Range(0.6f, 1f);
+
+                    inRadZoneLow = false;
+                    inRadZoneMed = false;
+                    inRadZoneHigh = false;
+
+                    if (inRadZoneExtr == false)
+                    {
+                        _sound.clip = _radExtr;
+                        _sound.Play();
+                        inRadZoneExtr = true;
+                    }
+                }
             }
         }
     }
@@ -99,28 +117,30 @@ public class ActivateRadEffect : MonoBehaviour {
     {
         if (col.gameObject.tag == "Player")
         {
-            sound.Stop();
-            RadiationLow.enabled = true;
-            RadiationMedium.enabled = true;
-            RadiationHigh.enabled = true;
-            grain.intensity = 0f;
-            ppp.grain.settings = grain;
-            aberration.intensity = 0f;
-            ppp.chromaticAberration.settings = aberration;
+            inRadZoneLow = false;
+            inRadZoneMed = false;
+            inRadZoneHigh = false;
+            inRadZoneExtr = false;
+            _sound.Stop();
+            _grain.intensity = 0f;
+            _aberration.intensity = 0f;
+            _ppp.grain.settings = _grain;
+            _ppp.chromaticAberration.settings = _aberration;
         }
     }
 
-    private void Start()
+    private void Awake()
     {
-        sound.Stop();
-        grain = ppp.grain.settings;
-        aberration = ppp.chromaticAberration.settings;
+        _sound.loop = true;
+        _sound.Stop();
+        _grain = _ppp.grain.settings;
+        _aberration = _ppp.chromaticAberration.settings;
 
         // Prevents bug in editor
-        grain.intensity = 0f;
-        ppp.grain.settings = grain;
-        aberration.intensity = 0f;
-        ppp.chromaticAberration.settings = aberration;
+        _grain.intensity = 0f;
+        _aberration.intensity = 0f;
+        _ppp.grain.settings = _grain;
+        _ppp.chromaticAberration.settings = _aberration;
     }
 }
 
