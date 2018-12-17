975811a0f9d976c76ab72316fbcdc2b1e2b2154b
synpse <tfaon@sapo.pt>
2018-12-17 20:13:30 +0000

    Added NPC

---
+++ Geiger's Ticking/Assets/DialogueManager.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class DialogueManager : MonoBehaviour {
+
+    public Text nameText;
+    public Text dialogueText;
+
+    public GameObject _consoleScreen;
+
+    private Queue<string> sentences;
+
+	void Start ()
+    {
+        sentences = new Queue<string>();
+	}
+
+    public void StartDialogue(Dialogue dialogue)
+    {
+        Debug.Log("Starting console dialogue...");
+
+        nameText.text = dialogue.name;
+
+        sentences.Clear();
+
+        foreach (string sentence in dialogue.sentences)
+        {
+            sentences.Enqueue(sentence);
+        }
+
+        DisplayNextSentence();
+    }
+
+    public void DisplayNextSentence()
+    {
+        if (sentences.Count == 0)
+        {
+            EndDialogue();
+            return;
+        }
+
+        string sentence = sentences.Dequeue();
+        StopAllCoroutines();
+        StartCoroutine(TypeSentence(sentence));
+    }
+
+    IEnumerator TypeSentence (string sentence)
+    {
+        dialogueText.text = "";
+        foreach (char letter in sentence.ToCharArray())
+        {
+            dialogueText.text += letter;
+            yield return null;
+        }
+    }
+	
+    void EndDialogue()
+    {
+        _consoleScreen.SetActive(false);
+        Debug.Log("Ended console dialogue.");
+    }
+}
---
+++ Geiger's Ticking/Assets/StartConsole.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class StartConsole : MonoBehaviour {
+
+    public GameObject _consoleScreen;
+
+    public GameObject _console;
+
+    private Interactible console;
+
+    public GameObject _player;
+
+    private bool ended;
+
+    void Start ()
+    {
+        console = _console.gameObject.GetComponent<Interactible>();
+        _consoleScreen.SetActive(false);
+    }
+	
+	void Update ()
+    {
+        if (!ended)
+        {
+            Check();
+        }
+
+        // If console is active
+        if (_consoleScreen.gameObject.activeInHierarchy)
+        {
+            Cursor.visible = true;
+            Cursor.lockState = CursorLockMode.None;
+            _player.GetComponent<RigidbodyFirstPersonController>().enabled = false;
+        }
+        else
+        {
+            Cursor.visible = false;
+            Cursor.lockState = CursorLockMode.Locked;
+            _player.GetComponent<RigidbodyFirstPersonController>().enabled = true;
+            ended = false;
+            console.interactiveOn = false;
+        }
+    }
+
+    private void Check()
+    {
+        if (console.interactiveOn == true)
+        {
+            _consoleScreen.SetActive(true);
+            FindObjectOfType<DialogueTrigger>().TriggerDialogue();
+            ended = true;
+        }
+    }
+}
