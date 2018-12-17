900058e3ab0caf3ef4bfbcd1c893b7368c98183b
synpse <tfaon@sapo.pt>
2018-12-17 16:40:12 +0000

    Changes to scene

---
+++ Geiger's Ticking/Assets/Scripts/Behaviours/OnDoorOpen.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnDoorOpen : MonoBehaviour {
+
+    public Rigidbody _ragdoll;
+    private bool opened;
+
+	private void Start ()
+    {
+        _ragdoll.isKinematic = true;
+    }
+	
+	private void Update ()
+    {
+		if (gameObject.GetComponent<Interactible>().interactiveOn == true
+            && opened == false)
+        {
+            _ragdoll.isKinematic = false;
+            opened = true;
+        }
+	}
+}
